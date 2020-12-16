c389c62c90b83d855be82518a0c275201402884f
Uno Yu <unosaan@gmail.com>
2020-12-16 10:44:28 +0900

    テストコミット

--- SauChat/HomeViewController.swift
+++ SauChat/HomeViewController.swift
@@ -12,7 +12,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("")
+        print("test")
 
         // Do any additional setup after loading the view.
     }
